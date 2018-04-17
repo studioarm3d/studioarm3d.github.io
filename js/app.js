5e8b05c1b41f47bd4188754c7eb6f1e35c8bda69
Mauricio Michels <mauricio@kugel.com.br>
2018-04-16 22:39:41 -0300

    initial

---
+++ js/app.js
@@ -0,0 +1,34 @@
+﻿$( document ).ready(function() {
+	var criaOptions = function() {
+		for(var i in filamentosExistentes){
+			var fila = filamentosExistentes[i];
+			if (fila.disponivel) {
+				$('#selectFila').append($('<option>', {
+					value: fila.key,
+					text: fila.name
+				}));
+			}
+		}
+	};
+	criaOptions();
+});
+
+var limpar = function() {
+	var gramas = $('#quantGramas').val();
+	var tempo = $('#tempoUtilizado').val();
+
+	$("#total").text("");
+};
+
+var calcular = function() {
+	var gramas = $('#quantGramas').val();
+	var tempo = $('#tempoUtilizado').val();
+	var fila = findExistFila("key", $('#selectFila').val());
+
+	var x1 = (gramas * procentPesoSoft) * fila.preco;
+	var x2 = (tempo * procentTempSoft) * precoMinutoMaquina;
+
+	var total = x1 + x2;
+	
+	$("#total").text("R$ " + total.toFixed(2));
+};
---
+++ js/db.js
@@ -0,0 +1,65 @@
+//Métodos Db
+var findExistFila = function(attr, keyWord){	
+	return filamentosExistentes.find(function(aux){return aux[attr] === keyWord})
+
+};	
+var intersection = function(a, b){				
+	var result = [];
+	for(var i in a){
+		if(b.includes(a[i])){
+			result.push(a[i]);
+		}
+	}
+	return result;
+};
+
+// Db
+var procentPesoSoft = 1.2;
+var procentTempSoft = 1.2;
+var precoMinutoMaquina = 0.006;
+
+var filamentosExistentes = [					
+	{
+		"name" 	: "3DFila - ABS Premium",		
+		"key"	: "3dFilaAp",
+		"disponivel" : true,
+		"preco"	: 0.13
+	},
+	{
+		"name" 	: "3DFila - ABS Premium Cristal",		
+		"key"	: "3dFilaApc",
+		"disponivel" : true,
+		"preco"	: 0.13
+	},
+	{
+		"name" 	: "3DFila - ABS Premium Cristal verde vagalume",		
+		"key"	: "3dFilaApcvv",
+		"disponivel" : true,
+		"preco"	: 0.18
+	},
+	{
+		"name" 	: "3DFila - PLA",		
+		"key"	: "3dFilaPla",
+		"disponivel" : true,
+		"preco"	: 0.17
+	},
+	{
+		"name" 	: "3DFila - Flexivel",		
+		"key"	: "3dFilaFlex",
+		"disponivel" : true,
+		"preco"	: 0.21
+	},
+	{
+		"name" 	: "3DFila - Flexivel",		
+		"key"	: "3dFilaFlex",
+		"disponivel" : true,
+		"preco"	: 0.21
+	},
+	{
+		"name" 	: "3DFila - PETG",		
+		"key"	: "3dFilaPetg",
+		"disponivel" : true,
+		"preco"	: 0.16
+	},
+	
+];
